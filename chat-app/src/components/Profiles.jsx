55ebf70a114265158b9513b93a4b870a2b738810
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-04-19 16:08:42 -0700

    started implementing logged in user specific logic, useOutletContext might cause issues with using logged_in_user id

--- chat-app/src/components/Profiles.jsx
+++ chat-app/src/components/Profiles.jsx
@@ -2,15 +2,46 @@ import userInformation from "./Profiles/helpers/sample_users";
 import Profile from "./Profiles/Profile";
 import "./Profiles.scss";
 
+import { useOutlet, useOutletContext } from "react-router-dom";
 
 export default function Profiles() {
   // State will keep track of which profiles to render, based on user logged in
   // We can use useOuletContext hook to obtain logged in users id
   // hardcoding users_languages
+	//Filter toggle button
 
-	console.log("userInformation", userInformation);
+	const { logged_in_user } = useOutletContext();
+	console.log("logged in user", logged_in_user)
+	
+	// Save the learning: true languages into array for the current_user --> language id
+	// Go thru usersMapped, filtering the "learning: false" matching current_users true learning languages
+	// Render these filteredUsers for this current user 
+
+	const loggedInId = logged_in_user.id;
+	console.log("loggedInId", loggedInId);
+
+	
+
+	const targetLanguages = userInformation.find(u => loggedInId === u.user.id).languages;
+
+	
+	console.log("target lang:", targetLanguages);
+
+	// convert a list of language objects, into list of language_ids
+
+	const learningLanguages = targetLanguages.filter((language) => language.learning === true);
+
+	// console.log("learningLanguages", learningLanguages);
+
+	const learningLanguagesIds = learningLanguages.map((language) => language.language_id);
+
+	console.log("learningLanguagesIds", learningLanguagesIds);
+	
 
 	const usersMapped = userInformation.map((information)=> {
+	
+
+
 		return (
 			<Profile
 				key={information.user.id}
@@ -28,7 +59,7 @@ export default function Profiles() {
   return (
     <main style={{ padding: "1rem 0" }}>
       <h2>Profiles</h2>
-      <ul class="cards" >{usersMapped}</ul>
+      <ul className="cards" >{usersMapped}</ul>
     </main>
   );
 }
