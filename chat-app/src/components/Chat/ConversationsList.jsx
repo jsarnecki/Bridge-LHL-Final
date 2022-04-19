81e0b0c27319a9981c9701cc80ceddeff8375dd8
tofutigerz <hfu3@ualberta.ca>
2022-04-19 16:06:47 -0600

    displays preview message

--- chat-app/src/components/Chat/ConversationsList.jsx
+++ chat-app/src/components/Chat/ConversationsList.jsx
@@ -1,10 +1,22 @@
 export default function ConversationsList(props) {
 	const mapConversations = (conversations, handleClick) => {
 		return conversations.map(conversation => {
+			const sortedMessages = conversation.messages.sort(
+				(a, b) => new Date(a.created_at) - new Date(b.created_at)
+			);
+			const lastMessage = sortedMessages[sortedMessages.length - 1];
+			console.log("sortedMessages", sortedMessages);
+			console.log("lastMessage", lastMessage);
 			return (
 				<li key={conversation.id} onClick={() => handleClick(conversation.id)}>
 					{conversation.friend_first_name} {conversation.friend_last_name} id:{" "}
 					{conversation.friend_id}
+					<br></br>
+					{lastMessage &&
+						(lastMessage.sender_id === conversation.friend_id
+							? conversation.friend_first_name + ":"
+							: "You:")}
+					{lastMessage && lastMessage.text}
 				</li>
 			);
 		});
