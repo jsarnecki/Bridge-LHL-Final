9d0cc7be6cce74f277fd3289ce1b6f9a8b635be5
tofutigerz <hfu3@ualberta.ca>
2022-04-19 12:23:50 -0600

    messages contain user name

--- chat-app/src/components/MessagesArea.jsx
+++ chat-app/src/components/MessagesArea.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import NewMessageForm from "./NewMessageForm";
 
 const MessagesArea = ({
-	conversation: { id, friend_id, messages },
+	conversation: {
+		id,
+		friend_id,
+		messages,
+		friend_first_name,
+		friend_last_name,
+	},
 	logged_in_user,
 }) => {
 	return (
 		<div className="messagesArea">
 			<h2>{friend_id}</h2>
-			<ul>{orderedMessages(messages)}</ul>
+			<ul>
+				{orderedMessages(
+					messages,
+					friend_id,
+					friend_first_name,
+					friend_last_name,
+					logged_in_user
+				)}
+			</ul>
 			<NewMessageForm
 				conversation_id={id}
 				logged_in_user={logged_in_user}
@@ -22,14 +36,27 @@ export default MessagesArea;
 
 // helpers
 
-const orderedMessages = messages => {
+const orderedMessages = (
+	messages,
+	friend_id,
+	friend_first_name,
+	friend_last_name,
+	logged_in_user
+) => {
 	const sortedMessages = messages.sort(
 		(a, b) => new Date(a.created_at) - new Date(b.created_at)
 	);
 	return sortedMessages.map(message => {
+		let sender;
+		if (message.sender_id === friend_id) {
+			sender = friend_first_name + " " + friend_last_name;
+		} else {
+			sender = logged_in_user.first_name + " " + logged_in_user.last_name;
+		}
+
 		return (
 			<li key={message.id}>
-				{message.sender_id}: {message.text}
+				{sender}: {message.text}
 			</li>
 		);
 	});
