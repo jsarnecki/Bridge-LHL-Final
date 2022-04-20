d2a023510074c0ac0f92ebbe2336b39093832a11
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-04-19 17:51:11 -0700

    add conditionl to handle undefined logged in user

--- chat-app/src/components/Profiles.jsx
+++ chat-app/src/components/Profiles.jsx
@@ -2,57 +2,69 @@ import userInformation from "./Profiles/helpers/sample_users";
 import Profile from "./Profiles/Profile";
 import "./Profiles.scss";
 
-import { useOutlet, useOutletContext } from "react-router-dom";
+import { useOutletContext } from "react-router-dom";
 
 export default function Profiles() {
   // State will keep track of which profiles to render, based on user logged in
   // We can use useOuletContext hook to obtain logged in users id
   // hardcoding users_languages
-	//Filter toggle button
+	// Filter toggle button
 
 	const { logged_in_user } = useOutletContext();
-	console.log("logged in user", logged_in_user)
 	
-	// Save the learning: true languages into array for the current_user --> language id
-	// Go thru usersMapped, filtering the "learning: false" matching current_users true learning languages
-	// Render these filteredUsers for this current user 
+	// // Save the learning: true languages into array for the current_user --> language id
+	// // Go thru usersMapped, filtering the "learning: false" matching current_users true learning languages
+	// // Render these filteredUsers for this current user 
 
-	const loggedInId = logged_in_user.id;
+	const loggedInId = logged_in_user.id || 1;
 	console.log("loggedInId", loggedInId);
 
-	
-
 	const targetLanguages = userInformation.find(u => loggedInId === u.user.id).languages;
-
-	
-	console.log("target lang:", targetLanguages);
-
 	// convert a list of language objects, into list of language_ids
-
 	const learningLanguages = targetLanguages.filter((language) => language.learning === true);
-
-	// console.log("learningLanguages", learningLanguages);
-
 	const learningLanguagesIds = learningLanguages.map((language) => language.language_id);
-
-	console.log("learningLanguagesIds", learningLanguagesIds);
 	
 
 	const usersMapped = userInformation.map((information)=> {
+	// save arr languages
+	// loop thru arr, push into new arr if learning == false
+	// comparing the ids to the learnignLanguagesIds
+	// if true, then render Profile
 	
+	let offeredLanguages = [];
+	for (const language of information.languages) {
+		if (language.learning === false) {
+			offeredLanguages.push(language.language_id);
+		}
+	}
+	
+	console.log("languages", offeredLanguages)
+	let match = false;
 
+	for (let lang of offeredLanguages) {
+		if (learningLanguagesIds.includes(lang)){
+			match = true;
+		}
+	}
 
-		return (
-			<Profile
-				key={information.user.id}
-				id={information.user.id}
-				firstName={information.user.first_name}
-				lastName={information.user.last_name}
-				image={information.user.image}
-				bio={information.user.bio}
-				languages={information.languages}			
-			/>
-		)
+	console.log("match", match, "languages", offeredLanguages);
+	console.log("learnLangId", learningLanguagesIds);
+	
+	if (match) {
+
+				return (
+					<Profile
+						key={information.user.id}
+						id={information.user.id}
+						firstName={information.user.first_name}
+						lastName={information.user.last_name}
+						image={information.user.image}
+						bio={information.user.bio}
+						languages={information.languages}			
+					/>
+				)
+		}
+
 	})
 
 
