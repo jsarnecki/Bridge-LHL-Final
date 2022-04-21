fafd4655c67c5eaca6a7ca6f6ab83bb00b6ff0f9
tofutigerz <hfu3@ualberta.ca>
2022-04-21 17:28:15 -0600

    validates not able to recreate conversation in reverse

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,4 +4,16 @@ class Conversation < ApplicationRecord
 	belongs_to :accepter, class_name: 'User'
 
 	validates_uniqueness_of :requester, scope: [:accepter]
+	validate :users_are_not_already_friends, on: :create
+
+	private
+
+	def users_are_not_already_friends
+		if Conversation.where(requester_id: accepter_id, accepter_id: requester_id)
+				.exists? ||
+				Conversation.where(requester_id: requester_id, accepter_id: accepter_id)
+					.exists?
+			self.errors.add(:requester_id, 'Already friends!')
+		end
+	end
 end
