f151e4782fa83f4240612666a03dc27151b28a3e
tofutigerz <hfu3@ualberta.ca>
2022-04-24 15:40:47 -0600

    adds put route to routes and creates edit button on hover message

--- chat-app/src/components/Chat/MessagesArea/MessagesListItem.jsx
+++ chat-app/src/components/Chat/MessagesArea/MessagesListItem.jsx
@@ -1,13 +1,26 @@
 import classNames from "classnames";
 import "./styles/MessagesListItem.scss";
+import { useState } from "react";
 export default function MessagesListItem(props) {
 	const { message, current_user, friend_first_name } = props;
+	const [hover, setHover] = useState(false);
+
+	const handleMouseOver = e => {
+		setHover(true);
+	};
+
+	const handleMouseOut = e => {
+		setHover(false);
+	};
+
 	return (
 		<li
 			className={classNames("messages-list-item", {
 				current_user,
 				initializer: message.initializer,
 			})}
+			onMouseOver={handleMouseOver}
+			onMouseOut={handleMouseOut}
 		>
 			{!current_user && (
 				<img
@@ -17,6 +30,10 @@ export default function MessagesListItem(props) {
 			)}
 
 			<p class="message-text">{message.text}</p>
+
+			{hover && !current_user && !message.edit && (
+				<i class="fa-solid fa-pen-to-square"></i>
+			)}
 		</li>
 	);
 }
