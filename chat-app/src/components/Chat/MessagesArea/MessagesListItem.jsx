e9956712762b2530558cd132dcbfeb89aa60b426
tofutigerz <hfu3@ualberta.ca>
2022-04-25 09:32:36 -0600

    adds comments explaining track changes

--- chat-app/src/components/Chat/MessagesArea/MessagesListItem.jsx
+++ chat-app/src/components/Chat/MessagesArea/MessagesListItem.jsx
@@ -25,25 +25,25 @@ export default function MessagesListItem(props) {
 		setEditing(false);
 	};
 
+	//Initialize variables to prevent undefined error is messaage.edit is false
 	let oldTextWithStrikes;
 	let newTextWithColour;
 
 	if (message.edit) {
-		console.log(`message id :${message.id}`);
 		const oldTextArr = message.text.split(" ");
 		const newTextArr = message.new_text.split(" ");
+		//returns arrays with indices of elements matched, added, and deleted, as well as result which contains all indices
 		const { matched, added, deleted, result } = findChange(
 			oldTextArr,
 			newTextArr
 		);
 
+		//Returns array of components with appropriate tags and classes for styling
 		oldTextWithStrikes = oldTextArr.map((word, i) => {
 			if (deleted.includes(i)) {
 				return (
-					<span>
-						<s className="deleted-text" key={i}>
-							{word}
-						</s>{" "}
+					<span key={i}>
+						<s className="deleted-text">{word}</s>{" "}
 					</span>
 				);
 			}
@@ -56,10 +56,14 @@ export default function MessagesListItem(props) {
 
 		newTextWithColour = newTextArr.map((word, i) => {
 			if (added.includes(i)) {
-				return <span className="added-text">{word + " "}</span>;
+				return (
+					<span key={i} className="added-text">
+						{word + " "}
+					</span>
+				);
 			}
 			return (
-				<span className="matched-text" key={i}>
+				<span key={i} className="matched-text">
 					{word + " "}
 				</span>
 			);
